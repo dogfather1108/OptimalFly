1af617c3d9c7af24088928c48a6f708ae235e0c1
dogfather1108 <dogfather1108@gmail.com>
2018-08-22 17:44:03 +0800

    初始化工程

---
+++ app.js
@@ -0,0 +1,11 @@
+//app.js
+import IndexRouteBtn from 'utils/infoConfig.js'
+App({
+  onLaunch: function () {
+    
+  },
+  globalData: {
+    userInfo: null,
+    IndexRouteBtn: IndexRouteBtn
+  }
+})
