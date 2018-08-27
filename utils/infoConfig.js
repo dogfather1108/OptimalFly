6e5ea5bebc5f8ac875d85ebbfc543e8720879e3b
dogfather1108 <dogfather1108@gmail.com>
2018-08-27 23:09:59 +0800

    首页完成

--- utils/infoConfig.js
+++ utils/infoConfig.js
@@ -1,27 +1,27 @@
 export const IndexRouteBtn = [
   {
-    img:'/pages/resources/girl.png',
+    img:'/pages/resources/bg_06.png',
     title:'洗车专题',
     tip:'为您提供专业的洗车服务',
     btn:'点击查看',
     key:'1'
   },
   {
-    img: '/pages/resources/girl.png',
+    img: '/pages/resources/bg_03.png',
     title: '任务专题',
     tip: '轻轻松松赚取任务红利',
     btn: '点击查看',
     key: '2'
   },
   {
-    img: '/pages/resources/girl.png',
+    img: '/pages/resources/bg_02.png',
     title: '车主认证',
     tip: '认证后即可享受多项权益',
     btn: '点击认证',
     key: '3'
   },
   {
-    img: '/pages/resources/girl.png',
+    img: '/pages/resources/bg_07.png',
     title: '关于我们',
     tip: '优飞-专业资质值得托付',
     btn: '点击了解',
